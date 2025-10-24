32e7460ee46450e420c9293d1146bc782bbcdea8
CodeX60 <151678852+AzizSaddique@users.noreply.github.com>
2025-10-24 15:48:18 -0700

    next

--- App.tsx
+++ App.tsx
@@ -1,45 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+import Flatcard from './components/Flatcard'
+import Elivatedcard from './components/Elivatedcard'
+import Fancycard from './components/Fancycard'
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+const App = () => {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+    <View>
+      <ScrollView>
+      <Flatcard/>
+      <Elivatedcard/>
+      <Fancycard/>
+      </ScrollView>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
+export default App
